71b1f106d2607318fbad03132489fab068a9547d
abhinav.aa.anand <abhinav.aa.anand@mercedes-benz.com>
2023-07-11 10:31:07 +0530

    Added add todo structure

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,21 @@ const todo_asc = computed(() => todos.value.sort((a, b) => {
   return b.createdAt - a.createdAt
 }))
 
+const addTodo = () => {
+  if (input_content.value && input_category.value) {
+    todos.value.push({
+      content: input_content.value,
+      category: input_category.value,
+      createdAt: Date.now()
+    })
+    input_content.value = ''
+    input_category.value = null
+  }
+  else {
+    alert('Please fill in all fields')
+  }
+}
+
 watch(name, (newVal) => {
   localStorage.setItem('name', newVal)
 })
@@ -26,9 +41,53 @@ onMounted(() => {
   <main class ="app">
 
     <section class="greeting">
-      <h2 class = "greeting__title">
-        Hello, <input v-model="name" class="greeting__input" placeholder="Your name">
+      <h2 class = "title">
+        Hello, <input v-model="name" type="text" id="name" placeholder="Your name">
       </h2>
     </section>
+    
+    <section class="create-todo">
+			<h3>CREATE A TODO</h3>
+
+			<form id="new-todo-form" @submit.prevent="addTodo">
+				<h4>What's on your todo list?</h4>
+				<input 
+					type="text" 
+					name="content" 
+					id="content" 
+					placeholder="e.g. make a video"
+					v-model="input_content" />
+				
+				<h4>Pick a category</h4>
+				<div class="options">
+
+					<label>
+						<input 
+							type="radio" 
+							name="category" 
+							id="category1" 
+							value="business"
+							v-model="input_category" />
+						<span class="bubble business"></span>
+						<div>Business</div>
+					</label>
+
+					<label>
+						<input 
+							type="radio" 
+							name="category" 
+							id="category2" 
+							value="personal"
+							v-model="input_category" />
+						<span class="bubble personal"></span>
+						<div>Personal</div>
+					</label>
+
+				</div>
+
+				<input type="submit" value="Add todo" />
+			</form>
+		</section>
+
   </main>
 </template>

ab951763f4ceb132ff2656633d5a2ca0107ea043
abhinav.aa.anand <abhinav.aa.anand@mercedes-benz.com>
2023-07-11 10:19:23 +0530

    Added Name greeting

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref, onMounted, computed, watch} from 'vue'
+
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_category = ref(null)
+
+const todo_asc = computed(() => todos.value.sort((a, b) => {
+  return b.createdAt - a.createdAt
+}))
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || ''
+})
+
+</script>
+
+<template>
+
+  <main class ="app">
+
+    <section class="greeting">
+      <h2 class = "greeting__title">
+        Hello, <input v-model="name" class="greeting__input" placeholder="Your name">
+      </h2>
+    </section>
+  </main>
+</template>
